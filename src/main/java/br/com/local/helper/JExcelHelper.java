cf169589340094d115fa3cc5362b5a82c0827762
rodrigo resque <rbresque@stafanni.com>
2021-07-18 17:42:10 -0300

    criacao metodo read

--- src/main/java/br/com/local/helper/JExcelHelper.java
+++ src/main/java/br/com/local/helper/JExcelHelper.java
@@ -1,15 +1,36 @@
 package br.com.local.helper;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
 public class JExcelHelper {
 	
-	public void readExcel(String fileLocation) throws IOException {
+	public Map<Integer, List<String>> readExcel(String fileLocation) throws IOException, BiffException {
 
-		Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
+		Map<Integer, List<String>> data = new HashMap<Integer, List<String>>();
+		
+		Workbook workbook = Workbook.getWorkbook(new File(fileLocation));
+		Sheet sheet = workbook.getSheet(0);
+		int rows = sheet.getRows();
+		int columns = sheet.getColumns();
+		
+		for (int i = 0; i < rows; i++) {
+			data.put(i, new ArrayList<String>());
+			for (int j = 0; j < columns; j++) {
+				data.get(i)
+					.add(sheet.getCell(j, i)
+					.getContents());
+			}
+		}
+		return data;
 		
 	}
 
